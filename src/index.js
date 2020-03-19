f1f4283595be91206e082c7b354ebf6245d34301
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-03-19 17:09:21 +0600

    feat: brackets task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = str.split( '' );
+  var pos = 0;
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (arr[i] == bracketsConfig[j][0] && arr[i + 1] == bracketsConfig[j][1]) {
+        pos = i;
+        arr.splice(pos, 2);
+        i = i - (pos+1);
+      }
+    }
+    if (arr.length == 0){
+      return true;
+    }
+  }
+  return false;
 }
